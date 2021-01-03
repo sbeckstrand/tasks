54f2b88d8b13ecbab9215fdd67c3330e8612e6f0
Stephen Beckstrand <spbeckstrand@gmail.com>
2021-01-03 01:04:26 -0800

    Added default index view/url as well as setup initial migration with DB structure

---
+++ readme.md
@@ -0,0 +1,19 @@
+## Tasks
+
+Tasks is an app that can be used to create and track active tasks for each class during an active School Semester
+
+## Getting Started
+
+#### Step 1: Clone the repository
+
+#### Step 2: Utilize existing migrations
+
+```
+python manage.py migrate
+```
+
+#### Step 3: Run the Application
+
+```
+python manage.py runserver
+```
---
+++ tasks/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.utils import timezone
+
+# Models for Catigorization
+class Term(models.Model):
+    name = models.CharField(max_length=200)
+    endDate = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+    def is_active(self):
+        return self.endDate > timezone.now()
+
+class Professor(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+    prof = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    term = models.ForeignKey(Term, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+# Models for actual Tasks
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    priority = models.IntegerField(max_length=1)
+    completed = models.BooleanField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    color = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Subtask(models.Model):
+    content = models.CharField(max_length=200)
+    completed = models.BooleanField()
+    parent = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+class Note(models.Model):
+    content = models.CharField(max_length=500)
+    parent = models.ForeignKey(Task, on_delete=models.CASCADE)
